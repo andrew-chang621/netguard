7270b8d93ca2e38f1046ed185c14f24d154bea2c
Ethan Daniel <ethandaniel@berkeley.edu>
2019-02-16 20:52:04 -0800

    refactor run.py

--- device-daemon/run.py
+++ device-daemon/run.py
@@ -8,28 +8,30 @@ from watchdog.utils.dirsnapshot import DirectorySnapshotDiff, DirectorySnapshot
 PATH_TO_DIR = "###"
 FILENAME = "###"
 
+def countLines(path):
+    return sum(1 for line in open(path))
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = PATH_TO_DIR
-    line_count = sum(1 for line in open(path + "/" + FILENAME))
-    snapshot = DirectorySnapshot(path, recursive=True)
+    line_count = countLines(PATH_TO_DIR + "/" + FILENAME)
+    snapshot = DirectorySnapshot(PATH_TO_DIR, recursive=True)
     event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, PATH_TO_DIR, recursive=True)
     observer.start()
 
     try:
         while True:
-            g = DirectorySnapshotDiff(snapshot, DirectorySnapshot(path, recursive=True))
+            g = DirectorySnapshotDiff(snapshot, DirectorySnapshot(PATH_TO_DIR, recursive=True))
             for i in g.files_modified:
                 print(i)
-                if i == path + "/" + FILENAME:
-                    temp = sum(1 for line in open(path + "/" + FILENAME))
+                if i == PATH_TO_DIR + "/" + FILENAME:
+                    temp = countLines(PATH_TO_DIR + "/" + FILENAME)
                     print(temp - line_count)
                     line_count = temp
-            snapshot = DirectorySnapshot(path, recursive=True)
+            snapshot = DirectorySnapshot(PATH_TO_DIR, recursive=True)
             time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
