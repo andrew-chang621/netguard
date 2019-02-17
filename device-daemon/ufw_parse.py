6f613c534c524e26043d851cbee2af3ab9b36a0a
Paul Yakovlev <yvpavel@gmail.com>
2019-02-17 00:16:15 -0800

    unbroke the algorithm

--- device-daemon/ufw_parse.py
+++ device-daemon/ufw_parse.py
@@ -1,4 +1,4 @@
-def format_log_data(recent_network_calls):
+def format_log_data(testLine):
     """
     Takes in a log file ufw log file as an input and
     formats it into a Python dictionary.
@@ -12,28 +12,28 @@ def format_log_data(recent_network_calls):
 
     formatted_logs = []
 
-    for line in recent_network_calls:
+    for line in testLine:
         new_data = {}
-        new_data["date"] = line.split(" raspberrypi")[0]
+        # new_data["date"] = line.split(" raspberrypi")[0]
         line = line.split(" ")
 
-        for atribute in line:
-            if "SRC" in atribute:
-                new_data["source"] = atribute.split("=")[1]
-            elif "MAC" in atribute:
-                new_data["mac"] = atribute.split("=")[1]
-            elif "PROTO" in atribute:
-                new_data["protocol"] = atribute.split("=")[1]
-            elif "LEN" in atribute:
-                new_data["packetLength"] = atribute.split("=")[1]
+        # print(line)
+
+        for element in line:
+            if "SRC" in element:
+                new_data["source"] = element.split("=")[1]
+            elif "MAC" in element:
+                new_data["mac"] = element.split("=")[1]
+            elif "PROTO" in element:
+                new_data["protocol"] = element.split("=")[1]
+            elif "LEN" in element:
+                new_data["packetLength"] = element.split("=")[1]
 
         formatted_logs.append(new_data)
 
-    return formatted_logs
+    print(formatted_logs)
 
 
 if __name__ == "__main__":
-    logs_file_path = "./test-logs.txt"
-
-    for log in format_log_data(logs_file_path):
-        print(log)
+    testLine = ["Feb 17 07:25:57 raspberrypi kernel: [ 1350.091508] [UFW BLOCK] IN=wlan0 OUT= MAC=b8:27:eb:bb:4a:3f:d4:61:9d:0b:a4:e6:08:00 SRC=172.20.10.2 DST=172.20.10.4 LEN=64 TOS=0x00 PREC=0x00 TTL=255 ID=0 DF PROTO=TCP SPT=55372 DPT=8290 WINDOW=65535 RES=0x00 SYN URGP=0"]
+    format_log_data(testLine)
